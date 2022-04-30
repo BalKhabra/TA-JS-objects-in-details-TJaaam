f1866e5a005f1367f97e5b72aed67cc9cba8956d
Baljit Khabra <baljitkhabra99@gmail.com>
2022-04-29 18:54:18 -0700

    different pattern

--- block-BJaahu/code/index.js
+++ block-BJaahu/code/index.js
@@ -1,30 +1,29 @@
-let userMethods = {
-    increaseScore: function(value = 1){
-        this.score = this.score + value;
-        return this.score;
+//prototype pattern
+
+let questionMethods = {
+    isAnswerCorrect: function(index){
+        return index === this.correctAnswerIndex;
         },
-    decreaseScore: function(value = 1){
-        this.score = this.score - value;
-        return this.score;
-        },
-    changeName: function(name){
-        this.name = name;
-        return this.name;
+    getCorrectAnswer: function(){
+        return this.options[this.correctAnswerIndex]
         }
 }
 
-
-let user1 = new createUser("sam", 0);
-let user2 = new createUser("cam");
-
 function createQuestion (title, options, correctAnswerIndex = 0){
-    let question = Object.create(createQuestion.prototype);
+    let question = Object.create(questionMethods.prototype);
     this.title = title;
     this.options = options;
     this.correctAnswerIndex = correctAnswerIndex;
     return question;
 }
 
+//psuedoclassical pattern
+
+function CreateQuestion (title, options, correctAnswerIndex = 0){
+    this.title = title;
+    this.options = options;
+    this.correctAnswerIndex = correctAnswerIndex;
+}
 
 CreateQuestion.prototype = {
     isAnswerCorrect: function(index){
@@ -34,30 +33,49 @@ CreateQuestion.prototype = {
         return this.options[this.correctAnswerIndex]
         }
 };
-
-
-
-let title = "Where is the capital of Jordan";
-let options = ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"];
-let correctAnswerIndex = 1;
-
-let title = "Where is the capital of Jamaica";
-let options = ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"];
-let correctAnswerIndex = 2;
-
-const firstQuestion = createQuestion (
+const firstQuestion = new CreateQuestion (
     "Where is the capital of Jordan",
     ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"],
      1
 );
 
-const secondQuestion = createQuestion (
+const secondQuestion = new CreateQuestion (
+    "Where is the capital of Jamaica",
+    ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"],
+     2
+);
+
+//psuedoclassical pattern
+class CreateQuestion {
+    constructor(title, options, correctAnswerIndex = 0){
+    this.title = title;
+    this.options = options;
+    this.correctAnswerIndex = correctAnswerIndex;
+}}
+
+CreateQuestion.prototype = {
+    isAnswerCorrect: function(index){
+        return index === this.correctAnswerIndex;
+        },
+    getCorrectAnswer: function(){
+        return this.options[this.correctAnswerIndex]
+        }
+};
+const firstQuestion = new CreateQuestion (
+    "Where is the capital of Jordan",
+    ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"],
+     1
+);
+
+const secondQuestion = new CreateQuestion (
     "Where is the capital of Jamaica",
     ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"],
      2
 );
 
 
+
+// class pattern
 
 class Question {
     constructor (title, options, correctAnswerIndex = 0){
@@ -73,3 +91,14 @@ class Question {
         }
 }
  
+const firstQuestion = new CreateQuestion (
+    "Where is the capital of Jordan",
+    ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"],
+     1
+);
+
+const secondQuestion = new CreateQuestion (
+    "Where is the capital of Jamaica",
+    ["Tashkent', 'Amaan', 'Kuwait City', 'Nairobi"],
+     2
+);
