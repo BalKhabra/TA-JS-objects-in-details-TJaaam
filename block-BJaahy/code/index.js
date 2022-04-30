f37ac5994e7bf9923230c75478b8f2aab3029bb8
Baljit Khabra <baljitkhabra99@gmail.com>
2022-04-29 23:10:38 -0700

    class

--- block-BJaahy/code/index.js
+++ block-BJaahy/code/index.js
@@ -1,22 +1,80 @@
 class Square {
     constructor (side) {
-        this.width = width;
-        this.height = height;
+        this.width = side;
+        this.height = side;
     }
+    description (){
+        alert `The square is ${this.width} x ${this.height}`
+    }
+    calcArea (){
+        return this.width * this.height;
+    }
+    get area(){
+        return this.width * this.height
+    } 
+    set area (value){
+        let side = Math.sqrt(value);
+        this.width = side;
+        this.height = side;
+    }
+    static isEqual (a,b) {
+        return (a.width * a.height) === (b.width * b.height) 
 }
-this.description = function (){
-    alert `The square is ${width} x ${height}`
 }
-this.calcArea = function (){
-    return (width * height);
+
+class Square {
+    constructor (side) {
+        this.width = side;
+        this.height = side;
+        this.numberOfTimes = 0;
+    }
+    description (){
+        alert `The square is ${this.width} x ${this.height}`
+    }
+    calcArea (){
+        return this.width * this.height;
+    }
+    get area(){
+        this.numberOfTimes++;
+        if (this.numberOfTimes >= 4){
+            alert (`Upper Limit Reached!`)
+        } else {
+            return this.width * this.height;
+        }
+    } 
+    set area (value){
+        let side = Math.sqrt(value);
+        this.width = side;
+        this.height = side;
+    }
+    static isEqual (a,b) {
+        return a.area === b.area;
 }
+}
+    
+
 
 
 
 class User {
     constructor (firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
-    fullName = function (){
-        return (`${firstName} ${lastName}`);
+    get fullName (){
+        return (`${this.firstName} ${this.lastName}`);
+    }
+    set fullName (name){
+        if(name.length < 5){
+            alert(`Full name should be more than 5 characters`)
+        } else {
+        let firstName = name.split(' ')[0]
+        let lastName = name.split(' ')[1]
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+    nameContains(str){
+        return `${this.firstName} ${this.lastName}`.contains(str);
     }
 }
