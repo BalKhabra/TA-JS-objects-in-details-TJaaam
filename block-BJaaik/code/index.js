a3079cbf8d745ca6b822119dc04a883a246d4aed
Baljit Khabra <baljitkhabra99@gmail.com>
2022-05-02 19:17:58 -0700

    completed inheritance tests

--- block-BJaaik/code/index.js
+++ block-BJaaik/code/index.js
@@ -1,62 +1,66 @@
 //inheritance
-
-function Person (name, age, gender) {
+class Person {
+    constructor(name, age, gender){
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
-
-Person.prototype = {
     eat(){
-
-    },
+        console.log(`${this.name} can eat!`);
+    }
     sleep(){
-        
-    },
+        console.log(`${this.name} can sleep!`);
+    }
     walk(){
-        
+        console.log(`${this.name} can walk!`);
+    }
+};
+
+
+class Player{
+    constructor (name, age, gender, sportsName){
+        super(name, age, gender);
+        this.sportsName = sportsName;
+}
+    play(){
+        console.log(`${this.name} can play!`);
+    }
+};
+
+
+
+class Teacher {
+    constructor(name, age, gender, instituteName){
+    super(name, age, gender);
+    this.instituteName = instituteName;
+}
+    teach(){
+        console.log(`${this.name} can teach!`);
+    }
+};
+
+
+class Artist {
+    constructor (name, age, gender, kind){
+    super(name, age, gender)
+    this.kind = kind;
+}
+    createArt(){
+        console.log(`${this.name} can create art like ${this.kind}!`);
     }
 }
-Object.prototypeOf(Person.prototype)
 
 
-function Player(name, age, gender, sportsName){
-    Person.call(this, name, age, gender)
-    obj.sportsName = sportsName;
-}
-Player.prototype = {
-    play: function(){
-
-    }
-}
-
-function Teacher (name, age, gender, instituteName){
-    Person.call(this, name, age, gender)
-    obj.instituteName = instituteName;
-}
-Teacher.prototype = {
-    teach: function(){
-
-    }
-}
-
-function Artist (name, age, gender, kind){
-    Person.call(this, name, age, gender)
-    obj.kind = kind;
-}
-Artist.prototype = {
-    createArt: function(){
-
-    }
-}
-
-
-function Cricketer (name, age, gender, sportsName, teamName){
-    Person.call(this, name, age, gender)
+class Cricketer extends Player {
+    constructor(name, age, gender, sportsName, teamName){
+    super(name, age, gender)
     obj.teamName = teamName;
 }
-Cricketer.prototype = {
     playCricker(){
-
+        console.log(`${this.teamName} can play cricket`);
     }
 }
+
+
+
+
